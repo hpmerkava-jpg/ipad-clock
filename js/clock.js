4373d7f607e36d8084496f644ddd6a4b356af9a7
Raptor <hpmerkava@gmail.com>
2026-07-27 19:14:49 +0200

    Fix iPad icon

--- js/clock.js
+++ js/clock.js
@@ -1,27 +1,36 @@
-function updateClock(){
+function updateClock() {
 
     const now = new Date();
 
     document.getElementById("time").textContent =
-        now.toLocaleTimeString("hr-HR",{
-
-            hour:"2-digit",
-            minute:"2-digit"
-
+        now.toLocaleTimeString("hr-HR", {
+            hour: "2-digit",
+            minute: "2-digit"
         });
 
-    document.getElementById("date").textContent =
-        now.toLocaleDateString("hr-HR",{
+    const dayDateInfo = document.getElementById("dayDateInfo");
 
-            weekday:"long",
-            day:"numeric",
-            month:"long",
-            year:"numeric"
+    if (dayDateInfo) {
 
-        });
+        const days = [
+            "Nedjelja",
+            "Ponedjeljak",
+            "Utorak",
+            "Srijeda",
+            "Četvrtak",
+            "Petak",
+            "Subota"
+        ];
 
+        const date =
+            String(now.getDate()).padStart(2, "0") + "." +
+            String(now.getMonth() + 1).padStart(2, "0") + "." +
+            now.getFullYear() + ".";
+
+        dayDateInfo.textContent =
+            days[now.getDay()] + "  " + date;
+    }
 }
 
 updateClock();
-
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
